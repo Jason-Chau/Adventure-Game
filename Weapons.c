78ec40473aa74053d2422f58e757e32838b9075f
joych51 <joych51@naver.com>
2021-10-25 15:06:32 -0400

    Weapons Edited

--- Weapons.c
+++ Weapons.c
@@ -4,19 +4,22 @@
 
 
 typedef struct WEAPON_STRUCT {
+    char name[30];
     char class[20];
     char type[30];
     int required_STR;
     int add_STR;
 } WEAPON;
 
-WEAPON setClass(WEAPON w, char *class) {
-    strcpy(w.class, class);
-    return w;
-}
+// WEAPON setClass(WEAPON w, char *class) {
+//     strcpy(w.class, class);
+//     return w;
+// }
 
-WEAPON initWeapon(WEAPON w, char *class, char *type, int required_STR, int add_STR) {
-    w = setClass(w, class);
+WEAPON initWeapon(char *name, char *class, char *type, int required_STR, int add_STR) {
+    WEAPON w;
+    strcpy(w.name, name);
+    strcpy(w.class, class);
     strcpy(w.type, type);
     w.required_STR = required_STR;
     w.add_STR = add_STR;
@@ -25,6 +28,7 @@ WEAPON initWeapon(WEAPON w, char *class, char *type, int required_STR, int add_S
 
 void printWeapon(WEAPON w) {
     printf("***************************\n");
+    printf("Name: \t\t\t%s\n", w.name);
     printf("Class: \t\t\t%s\n", w.class);
     printf("Type: \t\t\t%s\n", w.type);
     printf("Required Strength: \t%d\n", w.required_STR);
@@ -33,9 +37,9 @@ void printWeapon(WEAPON w) {
 
 }
 int main() {
-    WEAPON Blade = initWeapon(Blade, "Warrior", "Sword", 50, 15);
-    WEAPON OakAxe = initWeapon(OakAxe, "Warrior", "Axe", 100, 25);
-    printWeapon(Blade);
-    printWeapon(OakAxe);
+    WEAPON GodButcher = initWeapon("God Butcher", "Warrior", "Sword", 50, 15);
+    WEAPON LumberAxe = initWeapon("Lumber Axe", "Warrior", "Axe", 100, 25);
+    printWeapon(GodButcher);
+    printWeapon(LumberAxe);
 
 }
