4bee032f97f04d8280548880c5db1b5b8593407d
joych51 <joych51@naver.com>
2021-10-26 10:41:42 -0400

    Weapons edited v2

--- Weapons.c
+++ Weapons.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "Weapons.h"
+#include "characterStats.h"
 
-
-typedef struct WEAPON_STRUCT {
-    char name[30];
-    char class[20];
-    char type[30];
-    int required_STR;
-    int add_STR;
-} WEAPON;
-
-WEAPON initWeapon(char *name, char *class, char *type, int required_STR, int add_STR) {
+WEAPON initWeapon(char *name, char *required_Class, char *type, int required_STR, int add_STR) {
     WEAPON w;
     strcpy(w.name, name);
-    strcpy(w.class, class);
+    strcpy(w.required_Class, required_Class);
     strcpy(w.type, type);
     w.required_STR = required_STR;
     w.add_STR = add_STR;
@@ -22,22 +15,26 @@ WEAPON initWeapon(char *name, char *class, char *type, int required_STR, int add
 }
 
 void printWeapon(WEAPON w) {
-    printf("***************************\n");
+    printf("********************************\n");
     printf("Name: \t\t\t%s\n", w.name);
-    printf("Class: \t\t\t%s\n", w.class);
+    printf("Class: \t\t\t%s\n", w.required_Class);
     printf("Type: \t\t\t%s\n", w.type);
     printf("Required Strength: \t%d\n", w.required_STR);
     printf("+ Strength: \t\t%d\n\n", w.add_STR);
-    printf("***************************\n");
+    printf("********************************\n");
 
     // Need to dynamically print the required stat (DEX / STR / INT) depending on the classes, and additional stats too.
 }
 
-int main() {
-    WEAPON GodButcher = initWeapon("God Butcher", "Warrior", "Sword", 50, 15);
-    WEAPON LumberAxe = initWeapon("Lumber Axe", "Warrior", "Axe", 100, 25);
-    printWeapon(GodButcher);
-    printWeapon(LumberAxe);
+Stats wearWeapon(WEAPON w, Stats s) {
+    s.strength += w.add_STR;
+    return s;
+}
+
+Stats swapWeapon(WEAPON old, WEAPON new, Stats s) {
+    s.strength -= old.add_STR;
+    s.strength += new.add_STR;
+    return s;
 }
 
 // Once player character wears a certain weapons or armors, implement the function to add corresponding additional stats to the character.
