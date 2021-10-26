6d66e20caab6ff8b979ed73092d17c9c0e1d2e92
TimHake <timhake@hotmail.com>
2021-10-26 14:38:27 -0400

    Fixed inventory reference syntax

--- Inventory.h
+++ Inventory.h
@@ -1,9 +1,9 @@
 #ifndef INVENTORY_H_
 #define INVENTORY_H_
 
-#include "Weapons.c"
-//#include "Armors.c"
-//#include "Consumables.c"
+#include "Weapons.h"
+#include "Armors.h"
+#include "Consumables.h"
 
 typedef struct Inventory_struct {
     struct WEAPON* invWeapon[50];
@@ -13,6 +13,9 @@ typedef struct Inventory_struct {
 
 void CreateInventory(INVENTORY* inventory, WEAPON* weapons[50], ARMOR* armors[50], CONSUMABLE* consumables[50]);
 void DisplayInventory(WEAPON* weapons[50], ARMOR* armors[50], CONSUMABLE* consumables[50]);
+void AddWeapon(WEAPON* w, INVENTORY* inventory);
+void AddArmor(ARMOR* a, INVENTORY* inventory);
+void AddConsumable(CONSUMABLE* c, INVENTORY* inventory);
 void RemoveItem(char itemName[50], int itemType, INVENTORY* inventory);
 void RemoveWeapon(WEAPON* weapons[50], int target);
 void RemoveArmor(ARMOR* armors[50], int target);
