e7cf90fa3613816f0a1cd7cc605aeca4a1989fad
Jason Chau <namquychau@gmail.com>
2021-10-27 14:59:06 -0400

    Create enumClasses.h

---
+++ enumClasses.h
@@ -0,0 +1,30 @@
+#ifndef ENUM_CLASSES_H
+#define ENUM_CLASSES_H
+
+typedef enum Classes  {
+  Warrior,
+  Mage,
+  Thief
+} Classes;
+
+typedef struct characterStats {
+  char name[50];
+  Classes class;
+  int health;
+  int mana;
+  int hitPoints;
+  int currentHP;
+  int armorClass;
+  int strength;
+  int dexterity;
+  int intelligence;
+  int hit;
+  int damage;
+  double trapDetection;
+} Stats;
+
+void DisplayStats(Stats *target);
+int SetName(Stats *target, char name[50]);
+Stats* newCharacter(Classes class, char name[50]);
+
+#endif
