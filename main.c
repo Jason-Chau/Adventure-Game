9f844e59f422d68c565e1511d38a447a0e0b2092
Jason Chau <namquychau@gmail.com>
2021-10-27 01:01:28 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "characterStats.h"
+//#include "characterStats.h"
+#include "enumClasses.h"
 
 int main() {
-    Stats Test = initCharacterStats("First Character", "Human", "Warrior", 20, 10, 15, 14, 13);
-    printCharacter(Test);
+    //Stats Test = initCharacterStats("First Character", "Human", "Warrior", 20, 10, 15, 14, 13);
+    //printCharacter(Test);
+
+    Stats *Test = newCharacter(Warrior, "First Character");
+    DisplayStats(Test);
 }
