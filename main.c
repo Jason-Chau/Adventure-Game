de10312f0e129ab5fbf027026b12ad3e432d167a
joych51 <69375095+joych51@users.noreply.github.com>
2021-10-28 10:48:58 -0400

    Map

--- main.c
+++ main.c
@@ -17,11 +17,31 @@ int main() {
     // Stats *Test3 = newCharacter(Thief, "Third Character");
     // DisplayStats(Test3);
 
-    INVENTORY *inv = (INVENTORY*) malloc(sizeof(INVENTORY));
+    // INVENTORY *inv = (INVENTORY*) malloc(sizeof(INVENTORY));
     
-    CreateInventory(inv);
-    WEAPON *LumberAxe = initWeapon("Lumber Axe", "Warriors", 0, 10, 20);
-    AddWeapon(LumberAxe, inv);
-    //wearWeapon(LumberAxe, Test1, inv);
-    DisplayInventory(inv);
+    // CreateInventory(inv);
+    // WEAPON *LumberAxe = initWeapon("Lumber Axe", "Warriors", 0, 10, 20);
+    // AddWeapon(LumberAxe, inv);
+    // //wearWeapon(LumberAxe, Test1, inv);
+    // DisplayInventory(inv);
+
+      /*
+    char c;
+    Map* map; 
+    map = (Map*)malloc(num_rooms * sizeof(Room*)); //allocating memory for all the rooms
+    CreateRandomMap(map); //creates a random map
+    Print_Full_Map(map); //prints it, will comment out for real version
+    printf("Enter your menu selection: ");
+    current_room = map->rooms[0]; //sets current room to the first one in array
+    //Look_Around(r);
+    scanf(" %c",&c);
+    while(c != 'q')
+    {
+        menu(c); //execute menu
+        //print menu
+        Look_Around(current_room); //function to look at possible room options
+        printf("Enter your new choice: \n");
+        scanf(" %c",&c);
+    } 
+    */
 }
